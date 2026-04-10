9315f2325598a5d74b444296485053a4080e82a0
Paddy Griffin <paddygriffin@gmail.com>
2026-04-10 01:48:23 +0100

    New - Building Project Page

--- theme/template-parts/content/content-home.php
+++ theme/template-parts/content/content-home.php
@@ -9,60 +9,74 @@
  */
 
 ?>
-<?php if ( ! empty( get_the_content() ) ) : ?>
-<section class=" bg-gray-200">
+<?php if (!empty(get_the_content())): ?>
+	<section class=" bg-gray-200">
 
 
-<div class="container text-center">
-<article id="post-<?php the_ID(); ?>" <?php post_class('py-10 md:py-20'); ?>>
+		<div class="container text-center">
+			<article id="post-<?php the_ID(); ?>" <?php post_class('py-10 md:py-20'); ?>>
 
-	<header class="entry-header hidden">
-		<?php
-		if (! is_front_page()) {
-			the_title('<h1 class="entry-title">', '</h1>');
-		} else {
-			the_title('<h2 class="entry-title">', '</h2>');
-		}
-		?>
-	</header><!-- .entry-header -->
+				<header class="entry-header hidden">
+					<?php
+					if (!is_front_page()) {
+						the_title('<h1 class="entry-title">', '</h1>');
+					} else {
+						the_title('<h2 class="entry-title">', '</h2>');
+					}
+					?>
+				</header><!-- .entry-header -->
 
-	<?php bcc_post_thumbnail(); ?>
+				<div class="grid md:grid-cols-2 items-center gap-10">
 
-	<div <?php bcc_content_class('entry-content'); ?>>
-		<?php
-		the_content();
+					<div class="[&_a]:!text-white [&_h2]:!font-bold [&_a]:hover:!text-white">
+						<div <?php bcc_content_class('entry-content'); ?>>
+							<?php
+							the_content();
 
-		wp_link_pages(
-			array(
-				'before' => '<div>' . __('Pages:', 'bcc'),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+							wp_link_pages(
+								array(
+									'before' => '<div>' . __('Pages:', 'bcc'),
+									'after' => '</div>',
+								)
+							);
+							?>
+						</div><!-- .entry-content -->
+					</div>
 
-	<?php if (get_edit_post_link()) : ?>
-		<footer class="entry-footer">
-			<?php
-			edit_post_link(
-				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers. */
-						__('Edit <span class="sr-only">%s</span>', 'bcc'),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
-				)
-			);
-			?>
-		</footer><!-- .entry-footer -->
-	<?php endif; ?>
 
-</article><!-- #post-<?php the_ID(); ?> -->
- </div>
- </section>
- <?php endif; ?>
+					<div>
+
+						<?php bcc_post_thumbnail(); ?>
+					</div>
+
+				</div>
+
+
+
+
+
+				<?php if (get_edit_post_link()): ?>
+					<footer class="entry-footer">
+						<?php
+						edit_post_link(
+							sprintf(
+								wp_kses(
+									/* translators: %s: Name of current post. Only visible to screen readers. */
+									__('Edit <span class="sr-only">%s</span>', 'bcc'),
+									array(
+										'span' => array(
+											'class' => array(),
+										),
+									)
+								),
+								get_the_title()
+							)
+						);
+						?>
+					</footer><!-- .entry-footer -->
+				<?php endif; ?>
+
+			</article><!-- #post-<?php the_ID(); ?> -->
+		</div>
+	</section>
+<?php endif; ?>
